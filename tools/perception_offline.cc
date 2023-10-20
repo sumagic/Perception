d489dd40ccbc4cabfb55891314ba6faf72a7e1ef
kx <2544995178@qq.com>
2023-10-21 06:56:58 +0800

    add val2017 8 images

--- tools/perception_offline.cc
+++ tools/perception_offline.cc
@@ -13,13 +13,19 @@ int main(int argc, char **argv)
 {
     LOG_INFO("start to process image: ");
 
-    YAML::Node config = YAML::LoadFile("config/perception_offline.yaml");
+    std::string yaml_file_path = "config/perception_offline.yaml";
+    if(!perception::IsFileExist(yaml_file_path)) {
+        LOG_ERROR("yaml file: %s is not exist", yaml_file_path.c_str());
+        return -1;
+    }
+
+    YAML::Node config = YAML::LoadFile(yaml_file_path);
     std::string image_source_dir = config["image_source_dir"].as<std::string>();
     if (!perception::IsFileExist(image_source_dir)) {
         LOG_ERROR("image source dir: %s is not exist", image_source_dir.c_str());
         return -1;
     }
-    
+
     std::vector<std::string> src_image_list = {};
     cv::glob(image_source_dir, src_image_list, false);
     for (auto &src_image : src_image_list) {
