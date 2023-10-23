0a5dcb0e8852a04e78531fcabaca74fed284af5e
kx <2544995178@qq.com>
2023-10-23 20:38:54 +0800

    compile x86 and aarch64 all success

--- src/utils/box.cc
+++ src/utils/box.cc
@@ -24,14 +24,14 @@ float PredBoxIoU(const PredBox& predbox1, const PredBox& predbox2)
 
 Status PredBox::Draw(cv::Mat& image, const std::vector<std::string>& class_names) const
 {
+    #ifdef USE_OPENCV
     cv::rectangle(image, box, ::cv::Scalar(0, 255, 0), 2);
 
     std::string text = class_names[id] + ": " + std::to_string(score);
     int baseLine = 0;
-    cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX,
-                                              0.5, 1, &baseLine);
-    
+    cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
     cv::putText(image, text, cv::Point(box.x, box.y-10), cv::FONT_HERSHEY_PLAIN, 8, CV_RGB(255, 0, 0));//红色字体注释
+    #endif // USE_OPENCV
     return SUCCESS;
 }
 
